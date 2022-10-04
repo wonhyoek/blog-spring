8e704d6cc39d54fb8149de18255e4d6d543310ec
wonhyoek <epahsejtmxm@naver.com>
2022-10-04 21:24:55 +0900

    configure yml

---
+++ src/main/java/com/cos/blog/test/Member.java
@@ -0,0 +1,13 @@
+package com.cos.blog.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
