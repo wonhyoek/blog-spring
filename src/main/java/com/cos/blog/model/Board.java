c6e773122d6c3949d4c59e26363f4becc343f810
wonhyoek <epahsejtmxm@naver.com>
2022-10-04 22:20:11 +0900

    created Board model

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,4 +1,30 @@
 package com.cos.blog.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,4 +1,7 @@
 package com.cos.blog.model;
 
+
+
 public class Reply {
+
 }
