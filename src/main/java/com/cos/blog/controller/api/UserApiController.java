cb7cf5a15a08372abf6bd6958ef80471a493e519
wonhyoek <epahsejtmxm@naver.com>
2022-10-05 21:42:37 +0900

    Add UserService and test UserService

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){
         System.out.println("save 호출");
-        return new ResponseDto<Integer>(HttpStatus.OK, 1);
+        user.setRoleType(RoleType.USER);
+        User result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 }
---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,19 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User 회원가입(User user){
+        return userRepository.save(user);
+    }
+}
