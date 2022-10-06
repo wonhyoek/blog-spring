6ba706695360402d6f1ec0acbb1e57c30387355e
wonhyoek <epahsejtmxm@naver.com>
2022-10-06 20:01:57 +0900

    Domain Reply Done

--- src/main/java/com/cos/blog/repository/ReplyRepository.java
+++ src/main/java/com/cos/blog/repository/ReplyRepository.java
@@ -9,6 +9,6 @@ import com.cos.blog.model.Reply;
 public interface ReplyRepository extends JpaRepository<Reply, Integer>{
 
     @Modifying
-    @Query(value="INSERT INTO reply(userId, boardId, content, createDate) VALUES(?1, ?2, ?3, now())", nativeQuery = true)
+    @Query(value="INSERT INTO Reply(userId, boardId, content, createDate) VALUES(?1, ?2, ?3, now())", nativeQuery = true)
     int mSave(int userId, int boardId, String content); // 업데이트된 행의 개수를 리턴해줌.
 }
