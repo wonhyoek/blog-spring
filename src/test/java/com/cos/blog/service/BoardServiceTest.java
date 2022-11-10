7dae91cbf0ed6374e90d859ab512084f562a7574
wonhyoek <epahsejtmxm@naver.com>
2022-11-11 00:35:08 +0900

    게시글 상세보기 테스트 성공

--- src/test/java/com/cos/blog/service/BoardServiceTest.java
+++ src/test/java/com/cos/blog/service/BoardServiceTest.java
@@ -2,12 +2,11 @@ package com.cos.blog.service;
 
 import com.cos.blog.dto.BoardListWrapperDTO;
 import com.cos.blog.dto.saveBoardDTO.SaveBoardReqDTO;
-import com.cos.blog.dto.boardResDTO;
+import com.cos.blog.dto.BoardResDTO;
 import com.cos.blog.model.Board;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,11 +16,12 @@ import org.springframework.data.domain.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardServiceTest {
@@ -60,7 +60,7 @@ public class BoardServiceTest {
         when(boardRepository.save(any())).thenReturn(board);
 
         //when
-        boardResDTO resDTO = boardService.글쓰기(reqDTO,user);
+        BoardResDTO resDTO = boardService.글쓰기(reqDTO,user);
 
         //then
         System.out.println(resDTO.getTitle());
@@ -94,5 +94,29 @@ public class BoardServiceTest {
         //then
         assertThat(boardList.get(1).getTitle()).isEqualTo("title");
         assertThat(boardList.get(1).getContent()).isEqualTo("content");
+    }
+
+    @Test
+    public void getOneBoard(){
+        //given
+        String title = "good";
+        String content = "good test";
+        int id = 1;
+        Board board = Board.builder().id(id).title(title).content(content).build();
+        Optional<Board> boardOP = Optional.of(board);
+
+        //stub
+        doReturn(boardOP).when(boardRepository).findById(id);
+        //doThrow(new Exception) 못 찾는 상황에서 사용
+
+        //when
+        BoardResDTO resDTO = boardService.글상세보기(id);
+
+        //then
+        System.out.println(title);
+        System.out.println(content);
+        assertThat(resDTO.getId()).isEqualTo(id);
+        assertThat(resDTO.getTitle()).isEqualTo(title);
+        assertThat(resDTO.getContent()).isEqualTo(content);
     }
 }
