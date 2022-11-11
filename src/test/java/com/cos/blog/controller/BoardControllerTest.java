582855ea645ad3842f1d60e2c941aab47185fb4d
wonhyoek <epahsejtmxm@naver.com>
2022-11-11 17:18:44 +0900

    BoardApiController.save 테스트 성공

--- src/test/java/com/cos/blog/controller/BoardControllerTest.java
+++ src/test/java/com/cos/blog/controller/BoardControllerTest.java
@@ -1,21 +1,69 @@
 package com.cos.blog.controller;
 
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.controller.api.BoardApiController;
+import com.cos.blog.dto.BoardResDTO;
+import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.dto.SaveBoardReqDTO;
+import com.cos.blog.model.User;
 import com.cos.blog.service.BoardService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
 
 @ExtendWith(MockitoExtension.class)
 public class BoardControllerTest {
    @InjectMocks
-    private BoardController boardController;
+    private BoardApiController boardApiController;
 
    @Mock
     private BoardService boardService;
 
-   
+   @Test
+    public void save(){
+       //given
+       User user = User.builder()
+               .id(1)
+               .email("a@a.com")
+               .password("asdf")
+               .build();
+
+       SaveBoardReqDTO reqDTO = new SaveBoardReqDTO();
+       reqDTO.setTitle("good");
+       reqDTO.setContent("good test");
+
+       PrincipalDetail principalDetail = new PrincipalDetail(user);
+
+       BoardResDTO resDTO = BoardResDTO
+               .builder()
+               .id(1)
+               .title("good")
+               .content("good test")
+               .build();
+       //stub
+        doReturn(resDTO).when(boardService).글쓰기(reqDTO, user);
+
+       //when
+       ResponseDto<BoardResDTO> responseDto = boardApiController.save(reqDTO, principalDetail);
+
+       //then
+       System.out.println(responseDto.getData().getTitle());
+       assertThat(responseDto.getData().getTitle()).isEqualTo("good");
+   }
 
 }
+
+    //given
+
+    //stub
+
+    //when
+
+    //then
