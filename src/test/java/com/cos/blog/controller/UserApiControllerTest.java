0cac8dbb5e3de4a81bd11e67523903bfb125c4ec
wonhyoek <epahsejtmxm@naver.com>
2022-11-11 23:37:22 +0900

    BoardController.update 테스트 성공

--- src/test/java/com/cos/blog/controller/UserApiControllerTest.java
+++ src/test/java/com/cos/blog/controller/UserApiControllerTest.java
@@ -3,15 +3,19 @@ package com.cos.blog.controller;
 import com.cos.blog.controller.api.UserApiController;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.dto.SaveUserReqDTO;
+import com.cos.blog.dto.UpdateUserReqDTO;
 import com.cos.blog.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserApiControllerTest {
@@ -19,6 +23,8 @@ public class UserApiControllerTest {
     private UserApiController userApiController;
     @Mock
     private UserService userService;
+    @Mock
+    private AuthenticationManager authenticationManager;
 
     @Test
     public void save(){
@@ -41,6 +47,32 @@ public class UserApiControllerTest {
         //then
         System.out.println(responseDto.getData());
         assertThat(responseDto.getData()).isEqualTo(username);
+    }
+
+    @Test
+    public void update(){
+        //given
+        int id = 1;
+        String username = "username";
+        String email = "email";
+        String password = "password";
+
+        UpdateUserReqDTO reqDTO = new UpdateUserReqDTO();
+        reqDTO.setId(id);
+        reqDTO.setEmail(email);
+        reqDTO.setPassword(password);
+        reqDTO.setUsername(username);
+
+        //stub
+        doReturn(email).when(userService).회원수정(reqDTO);
+        doReturn(null).when(authenticationManager).authenticate(any());
+
+        //when
+        ResponseDto<String> responseDto = userApiController.update(reqDTO);
+
+        //then
+        System.out.println(responseDto.getData());
+        assertThat(responseDto.getData()).isEqualTo(email);
     }
 }
     //given
