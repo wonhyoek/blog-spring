6b98339eed4f7fca2c25db1a1b0fd5cebaad70a5
wonhyoek <epahsejtmxm@naver.com>
2022-11-11 17:02:48 +0900

    Mocking을 위한 세팅

--- src/test/java/com/cos/blog/controller/BoardControllerTest.java
+++ src/test/java/com/cos/blog/controller/BoardControllerTest.java
@@ -1,7 +1,21 @@
 package com.cos.blog.controller;
 
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import com.cos.blog.service.BoardService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 public class BoardControllerTest {
+   @InjectMocks
+    private BoardController boardController;
+
+   @Mock
+    private BoardService boardService;
+
+   
+
 }
